f2ac857821a5cecdd66152275a241b8f1cc45671
Yan Xiaozhi (David) <66581587+david-eom@users.noreply.github.com>
2020-12-07 21:32:38 +0800

    Add files via upload

---
+++ day5/day5.py
@@ -0,0 +1,41 @@
+# Part 1
+highest = 0
+
+with open("day5.txt", "r") as f:
+	for line in f.readlines():
+		row = 0
+		column = 0
+		for i in range(7):
+			if line[i] == "B":
+				row += 2**(6 - i)
+		for j in range(3):
+			if line[j + 7] == "R":
+				column += 2**(2 - j)
+		seat_ID = row * 8 + column
+		if seat_ID > highest:
+			highest = seat_ID
+
+print(highest)
+
+
+# Part 2
+seats = []
+
+with open("day5.txt", "r") as f:
+	for line in f.readlines():
+		row = 0
+		column = 0
+		for i in range(7):
+			if line[i] == "B":
+				row += 2**(6 - i)
+		for j in range(3):
+			if line[j + 7] == "R":
+				column += 2**(2 - j)
+		seat_ID = row * 8 + column
+		seats.append(seat_ID)
+
+seats.sort()
+
+for i in range(len(seats) - 1):
+	if seats[i + 1] - seats[i] != 1:
+		print(seats[i] + 1)
